f3d083fded6aef29bd3b24f70f5c17bbcf36b236
YourName <youremail@example.com>
2026-03-12 17:56:02 +0530

    .\pom.xml

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class App {
+
+ @GetMapping("/")
+
+ public String home() {
+
+ return "DevOps Project Running";
+
+ }
+
+}
